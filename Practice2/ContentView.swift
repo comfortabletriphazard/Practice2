1d39851a5941aab7bf7fc503aa22a31ace3e3f8c
Michael <michael.castlewellan@outlook.com>
2022-06-17 17:26:13 +0100

    Name Idea Generator Prototype

--- Practice2/ContentView.swift
+++ Practice2/ContentView.swift
@@ -8,27 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var button  = "Get Your Title"
-    @State var x = ""
-    @State var y = ""
+    
+    var words = ["Dirty", "Saddistic", "Assertive", "Unconventional", "Irritating", "Princess", "Peasant", "Champion", "Eejit"]
+    @State private var selectedWord = "Saddistic"
+    
+    var words2 = ["Speller", "Complainer", "Menace", "Monstrosity", "Pig", "Trollop", "Silence", "Tumble"]
+    @State private var selectedWord2 = "Speller"
     
     var body: some View {
         NavigationView {
-            Form{
-                TextField("Enter a word", text: $x)
-                TextField("Enter a word", text: $y)
-            
-                Button(button) {
-                    button = x + " " + y
+            VStack {
+                Section {
+                Picker("Select a word", selection: $selectedWord) {
+                    ForEach(words, id: \.self) {
+                        Text($0)
+                    }
                 }
-            }
-            .navigationTitle("Title Brainstormer")
+                    Picker("Select a word", selection: $selectedWord2) {
+                        ForEach(words2, id: \.self) {
+                            Text($0)
+                        }
+                }
+                     Text("\n\n\(selectedWord)  "+"\(selectedWord2)\n")
+//            }
+            .navigationTitle("Name Brainstormer")
         }
     }
 }
+    }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
