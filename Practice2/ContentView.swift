71dea3eb172887ab3695016dfbd3c1dbd932d4c3
Michael <michael.castlewellan@outlook.com>
2022-06-17 14:23:10 +0100

    First commit

--- Practice2/ContentView.swift
+++ Practice2/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var button  = "Get Your Title"
+    @State var x = ""
+    @State var y = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form{
+                TextField("Enter a word", text: $x)
+                TextField("Enter a word", text: $y)
+            
+                Button(button) {
+                    button = x + " " + y
+                }
+            }
+            .navigationTitle("Title Brainstormer")
+        }
     }
 }
 
