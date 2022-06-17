2140a8162d40574bb7834a72433a3e5b6d76a66a
Michael <michael.castlewellan@outlook.com>
2022-06-17 13:51:09 +0100

    Initial Commit

---
+++ Practice2/Practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice2App.swift
+//  Practice2
+//
+//  Created by Michael McAlarney on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
